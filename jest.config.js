116b350175e76343587ed840836a36207adbcead
Elahe Goodarzi <el.goodarzi2010@gmail.com>
2025-03-27 14:37:20 +0100

    configure jest and testing library for implementing unit-tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+}
